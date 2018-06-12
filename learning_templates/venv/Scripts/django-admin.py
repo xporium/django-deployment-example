5178ea961060b2b97239ea2cd4679d03e9c82d80
dhavalxporium <dhavalse@gmail.com>
2018-06-12 16:43:34 +0530

    'first'

---
+++ learning_templates/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Envelope\Python\Django_Level_One\Django_Level_One\learning_templates\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
